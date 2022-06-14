8bd9158e2b96af4baf085e1c6c2628de7b88ca57
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2022-06-14 17:14:39 +0400

    Вроде верное

--- test.py
+++ test.py
@@ -9,42 +9,43 @@ import warnings
 
 def hank():
     N = 100
-    a1 = -0.00005
+    a1 = 0.
     b1 = 0.00005
-    b2 = a1
-    h = (b1 - a1)/N
     a = 0.
-    foc = 300
+    foc = 100
     b = 1/(2*foc)
-    c = 5.
+    c = 0.00025
     alpha = 2*np.pi
-    z = 100
-    hz = (500 - 100)/20
+    z = 70
+    hz = (130 - 70)/500
+    print(hz)
     k = 10000.
     wl = 0.0005
 
     list1 = []
     x = np.linspace(a1, b1, num=N)
-    zed = np.linspace(100, 500, num=20)
-    rad = 5.
-    r_axis = x*np.cos(alpha)
-    x4 = lambda r: np.exp(1j*(((-2*np.pi/wl * b * r*np.cos(alpha)) ** 2) + (2*np.pi/wl * c * r*np.cos(alpha))))
-    while(z < 500):
-        x2 = lambda r: (1j*k/z)*np.exp(1j*k*z)*np.exp(1j*((k*a*r)**3+(k*b*r)**2 + k*c*r))*np.exp(1j*k*(r)**2/(2*z))*r
-        f, err = integr.quad(x2, 0, np.inf)
+    zed = np.linspace(70, 130, num=500)
+    x4 = lambda r: np.exp(1j * k * r * ((-b) * r + c))
+    while(z < 130):
+        x2 = lambda r: (1j*k/z)*np.exp(1j*k*z)*np.exp(1j * k * r * ((-b) * r + c))*np.exp(1j*k*r*r/(2*z))*r
+        f, err = integr.quad(x2, 1, np.inf)
         list1.append(f)
         z = z + hz
     list2 = x4(x)
     print(len(list1))
     plt.subplot(131)
-    plt.plot(x, (list2))
+    plt.plot(x, np.angle(list2))
+    plt.title("Входное поле")
     plt.grid()
     plt.subplot(132)
     res = np.abs(list1)
+    res2 = np.fft.fftshift(res)
     plt.plot(zed, res)
+    plt.title("Амплитуда преобразования")
     plt.grid()
     plt.subplot(133)
-    plt.plot(zed, list1)
+    plt.plot(zed, np.angle(list1))
+    plt.title("Фаза преобразования")
     plt.grid()
     plt.show()
 
