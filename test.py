521dbd2e456692106e94359443628651a650fde5
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2022-06-16 10:54:44 +0400

    1 пикча при с = 0

--- test.py
+++ test.py
@@ -11,20 +11,20 @@ def hank():
     N = 100
     a1 = 0.
     b1 = 0.00005
-    foc = 300
+    foc = 100
     b = 1/(2*foc)
-    c = 10.
-    z = 0.1
-    hz = (550 - 0.1)/70
+    c = 0.
+    z = 70.
+    hz = (130 - 70.)/70
     #print(hz)
     k = 10000.
     #wl = 0.0005
 
     list1 = []
     x = np.linspace(a1, b1, num=N)
-    zed = np.linspace(0.1, 550, num=71)
+    zed = np.linspace(70., 130, num=70)
     x4 = lambda r: np.exp(1j * k * r * ((-b) * r + c))
-    while(z < 550):
+    while(z < 130):
         x2 = lambda r: (1j*k/z)*np.exp(1j*k*z)*np.exp(1j * k * r * ((-b) * r + c))*np.exp(1j*k*r*r/(2*z))*r
         f, err = integr.quad(x2, 1, np.inf)
         list1.append(f)
@@ -38,7 +38,7 @@ def hank():
     plt.subplot(132)
     res = np.abs(list1)
     res2 = np.fft.fftshift(res)
-    plt.plot(zed, res2)
+    plt.plot(zed, res)
     plt.title("Амплитуда преобразования")
     plt.grid()
     plt.subplot(133)
